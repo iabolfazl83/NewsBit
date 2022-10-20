b39414538f7cdbe6039b0bad6b78a5d070c27d58
iabolfazl83 <iabolfazl83@yahoo.com>
2022-10-20 21:13:22 +0330

    NewsBit second final

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,21 @@
-function showMenu() {
+const menuBtn = document.querySelector(".header-menu-burger-btn");
+const headerMenu = document.querySelector(".header-menu")
+let isOpen = false;
+
+menuBtn.addEventListener("click", function () {
+    isOpen = !isOpen;
     let headerMenu = document.querySelector(".header-menu");
-    headerMenu.classList.toggle("show");
-}
+
+    if (isOpen) {
+        headerMenu.classList.add("show");
+        menuBtn.classList.add("active");
+        document.querySelector("body").style.overflowY = "hidden";
+        document.querySelector("body").style.paddingRight = "17px";
+    } else {
+        headerMenu.classList.remove("show");
+        menuBtn.classList.remove("active");
+        document.querySelector("body").style.overflowY = "auto";
+        document.querySelector("body").style.paddingRight = "0";
+    }
+
+})
